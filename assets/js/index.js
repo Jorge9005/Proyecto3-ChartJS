9ef4144ba027febb05c4a78844ac6f478ba6a5b3
Jorge Sandoval <jorge9005@outlook.com>
2023-05-06 15:18:34 -0600

    Adicion de estilos con css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,62 +1,32 @@
-(()=>{
-    console.log("Entró a la función anónima");
-    const BASE_URL = "https://sample-api-29g5.onrender.com/";
+
+// funcion anonima y arrow function
+(() => {
+    console.log("Soy la función anónima");
+    const BASE_URL = "https://sample-api-29g5.onrender.com/libros";
     const myChart = document.getElementById('myChart').getContext('2d');
     const tablaLibros = document.getElementById('tablaLibros');
 
-    const txtNombre = document.getElementById('txtNombre');
-    const btnAgregar = document.getElementById('btnAgregar');
-    const btnEditar = document.getElementById('btnEditar');
-    const btnEliminar = document.getElementById('btnEliminar');
-
-    btnAgregar.addEventListener("click", ()=>{
-        console.log('Entró al botón Agregar');
-        console.log(txtNombre.value);
-
-        let nombre = txtNombre.value;
-        fetch(BASE_URL + "libros",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "aplication/json",
-            },
-            body: JSON.stringify({
-                nombre: nombre,
-                cantidad: cantidad
-            })
-        })
-        .then(response => response.json())
-        .then(response => {
-            document.location.reload(); //Refresca la página para que se actualicen los datos
-        })
-        .catch(error => console.log(error))
-    });
 
     const loadData = () => {
-        fetch(BASE_URL + "libros",
-        {
-            method: "GET"
-        })
-        .then(response => response.json())
+        axios.get(BASE_URL)
         .then(response => {
-            console.log(response.data);
-            let labels_for_chart = response.data.map((item) => {
+            console.log(response.data.data)
+            let dataReady = response.data.data;
+            let labels_for_chart = dataReady.map((item) => {
                 return item.title.toUpperCase();
             });
-
-            let data_for_chart = response.data.map((item) => {
+            let data_for_chart = dataReady.map((item) => {
                 return item.count;
             });
-
             const grafica = new Chart(myChart, {
                 type: 'bar',
                 data: {
                     labels: labels_for_chart,
                     datasets: [{
-                        label: 'Libros',
+                        label: 'Animales del zoologico',
                         data: data_for_chart,
                         fill: true,
-                        backgroundColor: '#00008B',
+                        backgroundColor: '#000000',
                         borderColor: '#00008B'
                     }]
                 },
@@ -70,17 +40,25 @@
             });
 
             tablaLibros.innerHTML = "";
-
-            for(const libro of response.results){
+            for (const animal of dataReady) {
                 let tr = `<tr>
-                    <td>${lirbo.title.toUpperCase()}</td>
+                    <td>${animal.title.toUpperCase()}</td>
                     <td>${animal.count}</td>
-                    </tr>
+                </tr>
                 `;
                 tablaLibros.innerHTML += tr;
             }
+
         })
-        .catch(error => console.log(error))
+            .catch(function (error) {
+                console.log("Error de axios");
+                console.log(error);
+            })
+            .finally(function () {
+                console.log("Finally de axios");
+            });
+
     }
+
     loadData();
 })()
